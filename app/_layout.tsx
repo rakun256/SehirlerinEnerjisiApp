135bc9273246407687b741d9f16c53d5a4f7a107
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2025-07-08 18:16:35 +0300

    add font loading and loading text to RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { Text } from "react-native";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Inter-Regular": require("../assets/fonts/Inter-Regular.ttf"),
+    "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
+    "Inter-Light": require("../assets/fonts/Inter-Light.ttf"),
+    "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
+    "Inter-Thin": require("../assets/fonts/Inter-Thin.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Yükleniyor...</Text>;
+  }
+  
   return <Stack />;
 }
